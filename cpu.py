129026b9c9fc10191ed0cdd934acd21e5249c33a
AHarman <alexanderjharman@gmail.com>
2015-10-31 22:22:07 +0000

    All working I think

--- cpu.py
+++ cpu.py
@@ -3,6 +3,20 @@ import numpy as np
 from state import *
 from executeFunctions import *
 
+def memToString():
+    global memory
+    string = "Memory:\n"
+    for i in range(10):
+        string += str(i) + ": " + format(int(memory[i]), "#02x") + "\n"
+    return string
+
+def regToString():
+    global registers
+    string = "Registers:\n"
+    for i in range(len(registers)):
+        string += str(i) + ": " + format(int(registers[i]), "#02x") + "\n"
+    return string
+
 def loadProgram(filename):
     with open(filename) as f:
         programIn = f.read()
@@ -12,25 +26,25 @@ def loadProgram(filename):
     instructionsIn = [x for x in programIn[1].split("\n") if x != ""]
 
     for i in range(len(memoryIn)):
-        memory[i] = np.int32(int(memoryIn[i], 2))
+        memory[i] = np.uint32(int(memoryIn[i], 2))
     for i in range(len(instructionsIn)):
-        instructions[i] = np.int32(int(instructionsIn[i], 2))
+        instructions[i] = np.uint32(int(instructionsIn[i], 2))
 
 
 def fetch():
-    return instructions[programCounter]
+    global programCounter
+    programCounter += 1
+    return instructions[programCounter - 1]
 
 def decode(instruction):
     instruction = format(int(instruction), "032b")
     opcode = np.uint8(int(instruction[:8], 2))
-    operands = None
-    immediate = None
+    immediate = 0
     
-    if opcode == 0x00:                              # No op
-        return 0x00, None, None
-    if opcode == 0xFF:                              # Terminate
-        return 0xFF, None, None
-    print "Number of registers is: "
+    if opcode == 0x00:                                          # No op
+        return 0x00, [], 0
+    if opcode == 0xFF:                                          # Terminate
+        return 0xFF, [], 0
     if ((opcode > 0x01 and opcode <= 0x0F) or                   # Instructions with 3 registers 
         (opcode | 1 == 0x25) or 
         (opcode | 1 == 0x2B) or 
@@ -63,19 +77,29 @@ def execute(opcode, operands, immediate):
     return
 
 def executeProgram():
+    logString = ""
+
     global programCounter
     opcode = 0
     while opcode != 0xFF:
         currentInstruction = fetch()
         (opcode, operands, immediate) = decode(currentInstruction)
-        print (format(int(opcode), "08b"), operands, immediate)
+        print (format(int(opcode), "#02X"), operands, format(int(immediate), "#02X"))
         execute(opcode, operands, immediate)
-        programCounter += 1
+
+        logString += instructionText[opcode] + "\t" +  str(operands) + "\t\t" + format(int(immediate), "#02X") + "\n"
+        logString += memToString() + "\n\n" + regToString() + "\n\n**********\n\n" 
+    
+    with open("log.out", 'w') as f:
+        f.write(logString)
     return
 
 def main():
     loadProgram(sys.argv[1])
     executeProgram()
+
+    print "Number stored in memory address 255: "
+    print hex(memory[255])
     return
 
 if __name__ == "__main__":
--- state.py
+++ state.py
@@ -1,6 +1,28 @@
 import numpy as np
-memory = np.zeros(1024, dtype=np.int32)
-instructions = np.zeros(1024, dtype=np.uint32)
-registers = np.zeros(16, dtype=np.int32)
+memory = np.zeros(256, dtype=np.uint32)
+instructions = np.zeros(256, dtype=np.uint32)
+registers = np.zeros(16, dtype=np.uint32)
 programCounter = np.uint32(0)
 
+# Used for debugging
+instructionText = {
+    0b00000000 : "NOP", 0b00000001 : "MEM",
+    0b00000010 : "ADD", 0b00000011 : "ADDI",
+    0b00000100 : "SUB", 0b00000101 : "SUBI",
+    0b00000110 : "MUL", 0b00000111 : "MULI",
+    0b00001000 : "AND", 0b00001001 : "ANDI",
+    0b00001010 : "OR",  0b00001011 : "ORI",
+    0b00001100 : "LS",  0b00001101 : "LSI",
+    0b00001110 : "RS",  0b00001111 : "RSI",
+    0b00100010 : "LD",  0b00100011 : "LDI",
+    0b00100100 : "LDA", 0b00100101 : "LDAI",
+    0b00100110 : "MV",  0b00100111 : "MVI",
+    0b00101000 : "ST",  0b00101001 : "STI",
+    0b00101010 : "STA", 0b00101011 : "STAI",
+    0b01000010 : "BR",  0b01000011 : "BRI",
+    0b01000100 : "BRE", 0b01000101 : "BREI",
+    0b01000110 : "BRN", 0b01000111 : "BRNI",
+    0b01001000 : "BRL", 0b01001001 : "BRLI",
+    0b01001010 : "BRG", 0b01001011 : "BRGI",
+    0b01001100 : "BRZ", 0b01001101 : "BRZI",
+    0b11111111 : "TRM"}
